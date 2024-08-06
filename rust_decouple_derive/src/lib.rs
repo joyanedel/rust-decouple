2802be7decf4f889b232c170be7e64c138bb91b0
joyanedel <joyanede@alumnos.inf.utfsm.cl>
2024-08-06 00:42:43 -0400

    Add vec support to proc macro

--- rust_decouple_derive/src/lib.rs
+++ rust_decouple_derive/src/lib.rs
@@ -1,9 +1,8 @@
-use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, DeriveInput, Ident, Type};
 
 #[proc_macro_derive(EnvVarParser)]
-pub fn derive_env_var_parser(input: TokenStream) -> TokenStream {
+pub fn derive_env_var_parser(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = &input.ident;
 
@@ -13,27 +12,27 @@ pub fn derive_env_var_parser(input: TokenStream) -> TokenStream {
                 .fields
                 .iter()
                 .filter_map(|f| {
-                    let f = f.clone();
-                    if f.ident.is_some() {
-                        Some((f.ident.unwrap(), f.ty))
+                    let ident = f.clone().ident;
+                    let ty = f.ty.clone();
+                    let ty_is_vec = f.ty.clone();
+                    if ident.as_ref().is_some() {
+                        Some((ident.unwrap(), ty, is_vec(ty_is_vec)))
                     } else {
                         None
                     }
                 })
                 .collect();
-
-            let field_names: Vec<_> = fields.iter().map(|(a, _)| a).collect();
-            let field_names_uppercase: Vec<_> = field_names
-                .iter()
-                .map(|f| f.to_string().to_uppercase())
-                .collect();
-            let field_types: Vec<_> = fields.iter().map(|(_, b)| b).collect();
+            let non_vec_fields: Vec<_> = fields.iter().filter(|(_, _, v)| !v).collect();
+            let non_vec_fields = gen_fields(non_vec_fields);
+            let vec_fields: Vec<_> = fields.iter().filter(|(_, _, v)| *v).collect();
+            let vec_fields = gen_fields(vec_fields);
 
             quote! {
                 impl EnvVarParser for #name {
                     fn parse() -> Self {
                         Self {
-                            #(#field_names: rust_decouple::core::Environment::from::<#field_types>(#field_names_uppercase, None),)*
+                            #non_vec_fields
+                            #vec_fields
                         }
                     }
                 }
@@ -43,4 +42,34 @@ pub fn derive_env_var_parser(input: TokenStream) -> TokenStream {
     };
 
     expanded.into()
+}
+
+/// this function must be improved
+fn is_vec(v: Type) -> bool {
+    let v = quote!(#v).to_string();
+    v.starts_with("Vec")
+}
+
+fn gen_fields(fields: Vec<&(Ident, Type, bool)>) -> proc_macro2::TokenStream {
+    if fields.len() == 0 {
+        return quote! {};
+    }
+
+    let field_names: Vec<_> = fields.iter().map(|(f, _, _)| f).collect();
+    let field_names_uppercase: Vec<_> = fields
+        .iter()
+        .map(|(f, _, _)| f.to_string().to_uppercase())
+        .collect();
+    let field_types: Vec<_> = fields.iter().map(|(_, t, _)| t).collect();
+    let is_vec = fields.iter().next().unwrap().2;
+
+    if is_vec {
+        quote! {
+            #(#field_names: rust_decouple::core::VecEnvironment::from(#field_names_uppercase, None) as #field_types,)*
+        }
+    } else {
+        quote! {
+            #(#field_names: rust_decouple::core::Environment::from::<#field_types>(#field_names_uppercase, None),)*
+        }
+    }
 }
