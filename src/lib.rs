566877c4e50d318789da404c349e56393302bc46
joyanedel <joyanede@alumnos.inf.utfsm.cl>
2024-08-07 11:55:12 -0400

    Restructure project to add env parser as a optional feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
-pub use rust_decouple_derive;
+// #[cfg(feature = "derive")]
+// pub use rust_decouple_derive;
+
 pub mod core;
 pub mod macros;
+mod traits;
+
+#[cfg(feature = "derive")]
+extern crate rust_decouple_derive;
+#[cfg(feature = "derive")]
+pub use rust_decouple_derive::EnvVarParser;
+#[cfg(feature = "derive")]
+pub use traits::EnvVarParser;
---
+++ src/traits.rs
@@ -0,0 +1,3 @@
+pub trait EnvVarParser {
+    fn parse() -> Self;
+}
